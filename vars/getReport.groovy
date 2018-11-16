bc21166d9f98b50b388f002d6844abd07fb6b381
uniseb123 <sebastian.wenta@uniteam.pl>
2018-11-16 16:13:42 +0100

    Update getReport.groovy

--- vars/getReport.groovy
+++ vars/getReport.groovy
@@ -8,16 +8,6 @@ def call(){
     String stableText = ""
     
     stableText += "<br><br><b>ROW 1</b>"
-    stableText += "<br><br><b>WINDOWS</b>"
-    stableText += "<br><a href='www.google.pl'>Logowanie</a>"
-    stableText += "<br><a href='www.google.pl'>Weryfikacja kontrahenta</a>"
-    stableText += "<br><a href='www.google.pl'>Zmiany na koncie</a>"
-    stableText += "<br><b>LINUX</b>"
-    stableText += "<br><a href='www.google.pl'>Logowanie SAP</a>"
-    stableText += "<br><a href='www.google.pl'>Ksiegowanie</a>"
-    stableText += "<br><a href='www.google.pl'>Zapisanie</a>"
-    
-    stableText += "<br><br><b>ROW 1</b>"
     stableText += "<br><b>WINDOWS</b>"
     stableText += "<br><a href='www.google.pl'>Logowanie</a>"
     stableText += "<br><a href='www.google.pl'>Weryfikacja kontrahenta</a>"
