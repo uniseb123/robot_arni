94622a735367cdfdf226ae30d198a969ddec27a1
uniseb123 <sebastian.wenta@uniteam.pl>
2018-12-04 17:04:55 +0100

    Update runRobot.groovy

--- vars/runRobot.groovy
+++ vars/runRobot.groovy
@@ -1,5 +1,4 @@
 def call(int currentRow){
     prepareDataBase(currentRow)
-    println "Database prepared for ${currentRow}"
     println bat(script:"runRobot.bat ${currentRow}", returnStdout: true)
 }
